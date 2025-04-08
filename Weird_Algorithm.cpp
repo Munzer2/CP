5179d203151baffe710eea002b3ce81cda696590
Munzer Mahmood <munzermahmood2014@gmail.com>
2025-04-08 21:48:16 +0600

    Solved

---
+++ Weird_Algorithm.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+using namespace std;
+
+void debug_vector(vector<ll>& a) {
+    for (auto x : a) cout << x << " ";
+    cout << "\n";
+    return;
+}
+
+void solve() {
+    ll n ; 
+    cin >> n ;
+    cout << n << " ";  
+    while(n != 1) {
+        n = ( n%2 ? n*3 + 1 : n/2);
+        cout << n << " ";  
+    }
+    cout << endl ; 
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll tt=1;
+    // cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
