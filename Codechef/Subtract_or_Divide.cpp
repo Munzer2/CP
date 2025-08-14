8e1984740942869cd9707b8edabf8f40910e15c9
Munzer Mahmood <munzermahmood2014@gmail.com>
2025-08-14 11:31:51 +0600

    Done

---
+++ Codechef/Subtract_or_Divide.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+using namespace std;
+
+void debug_vector(vector<ll>& a) {
+    for (auto x : a) cout << x << " ";
+    cout << "\n";
+    return;
+}
+
+void solve() {
+    ll n ; 
+    cin >> n; 
+    if(n%2) {
+        cout << (n+1)/2 << "\n";
+        return; 
+    }
+    cout << ((n/2)%2 ? (n/2 + 1)/2 : n/4 ) + n/2 << "\n";
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll tt;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
+
+///passed
