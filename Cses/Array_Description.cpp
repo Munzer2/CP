d2e250cfd82e4897d7210f4dec2f735c38a091fa
Munzer Mahmood <munzermahmood2014@gmail.com>
2025-09-17 23:10:23 +0600

    Dp

--- Cses/Array_Description.cpp
+++ Cses/Array_Description.cpp
@@ -2,6 +2,7 @@
 #define ll long long
 #define all(x) x.begin(), x.end()
 #define pb(x) push_back(x)
+#define FAST() ios::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
 
 void debug_vector(vector<ll>& a) {
@@ -10,37 +11,40 @@ void debug_vector(vector<ll>& a) {
     return;
 }
 
-
 ll MOD = 1e9 + 7;
 
 void solve() {
     ll n , m ; 
     cin >> n >> m; 
     vector< ll > a(n); 
-    ll mx = -1; 
     for(auto &x : a) { 
-        cin >> x; mx = max(mx, x); 
+        cin >> x;
     } 
-
-    vector< vector < ll >> dp(n+1,vector< ll >(mx+3,1)); 
-    for(ll i = 0; i < n; ++i) {
-        if(a[i-1] != 0) continue;
-        for(ll j = 1; j <= mx+2 ; ++j) {
-            
+    vector< vector< ll >> dp(n, vector< ll >(m+1,0));
+    for(ll i = 1; i <= m ; ++i) { 
+        dp[0][i] = (a[0] == 0 ? 1 : (i == a[0] ? 1 : 0));
+    }
+    for(ll i = 1 ; i < n; ++i) { 
+        if(a[i] != 0) { 
+            for(ll val = max(1LL,a[i]-1);  val <= min(m,a[i]+1); ++val) dp[i][a[i]] = (dp[i][a[i]] + dp[i-1][val])%MOD;
+            continue ; 
+        }
+        for(ll p = 1; p <= m; ++p) { 
+            for(ll val = max(p-1,1LL) ; val <= min(m,p+1); ++val) dp[i][p] = (dp[i][p] + dp[i-1][val])%MOD;
         }
     }
+    ll ans = 0; 
+    for(ll p = 1; p <= m ; ++p) ans = (ans + dp[n-1][p])%MOD;
+    cout << ans << "\n";  
     return;
 }
 
 /// dp[i][v] = number of ways to fill up positions 0...i and A[i] becomes v.
 
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll tt;
-    cin >> tt;
+    FAST();
+    ll tt = 1;
+    // cin >> tt;
     while (tt--) {
         solve();
     }
