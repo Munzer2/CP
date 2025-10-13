5f766d14f1ea46b9961dba5c0ac188b3f87f244a
Munzer Mahmood <munzermahmood2014@gmail.com>
2025-10-13 12:28:28 +0600

    Solved

---
+++ codeforces/C_Reverse_XOR.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define FAST() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+void debug_vector(vector<ll>& a) {
+    for (auto x : a) cout << x << " ";
+    cout << "\n";
+    return;
+}
+
+void solve() {
+    ll n ; 
+    cin >> n; 
+    if(!n) { 
+        cout << "YES\n"; return; 
+    }
+    while(n%2 == 0) n/=2; 
+    if(n == 1) { 
+        cout << "NO\n"; return;
+    }
+    auto check = [&](ll n)-> ll{
+        unsigned long long mask = 1;
+        ll _c = n;
+        while(_c>1) { 
+            _c/=2; mask <<= 1;
+        }
+        unsigned long long mask2 = 1;  
+        // cout << mask << "\n";
+        while(mask > mask2) { 
+            if(((mask2&n) && (mask&n)) || ((mask&n) == 0 && (mask2&n)== 0)) { 
+                mask2 <<= 1; mask >>= 1;
+            }
+            else return 0;
+        }
+        if(mask == mask2 && ( (mask&n) && (mask2&n))) return 0;
+        return 1;
+    };
+    cout << (check(n) ? "YES" : "NO") << "\n";
+    return; 
+}
+
+/// so I need a palindrome of even length. Right ?
+/// Good problem with a twist.
+/// accepted.
+
+int main() {
+    FAST();
+    ll tt;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
